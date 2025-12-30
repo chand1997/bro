186a8d92d46c3445fa54a7f3ef9ac6a58b8c3d93
chand1997 <chanduchallahalli@gmail.com>
2025-12-30 11:12:32 +0530

    bro

--- 80-web-alb/parameters.tf
+++ 80-web-alb/parameters.tf
@@ -3,5 +3,5 @@
 resource "aws_ssm_parameter" "frontend_listener_arn" {
   name  = "/${var.project_name}/${var.environment}/frontend_listener_arn"
   type  = "String"
-  value = data.aws_lb_listener.app_alb.arn
+  value = aws_lb_listener.web_alb.arn
 }
